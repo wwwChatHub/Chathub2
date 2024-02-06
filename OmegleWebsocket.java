2f29484ad9c5b04bffcd2214ede9d04342cc0cfa
wwwChatHub <159187858+wwwChatHub@users.noreply.github.com>
2024-02-06 15:55:47 -0500

    Create OmegleWebsocket.java

---
+++ OmegleWebsocket.java
@@ -0,0 +1,5 @@
+<dependency>
+    <groupId>org.eclipse.jetty.websocket</groupId>
+    <artifactId>websocket-server</artifactId>
+    <version>9.4.43.v20210629</version> <!-- Replace with the latest version -->
+</dependency>
