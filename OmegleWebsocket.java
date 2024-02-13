ad3a5ce0b950a2becf2ea2328d943ccda30a7b6f
wwwChatHub <159187858+wwwChatHub@users.noreply.github.com>
2024-02-13 04:52:01 -0500

    Delete OmegleWebsocket.java

--- OmegleWebsocket.java
+++
@@ -1,6 +0,0 @@
-    <dependency>
-        <groupId>org.eclipse.jetty</groupId>
-        <artifactId>jetty-server</artifactId>
-        <version>9.4.43.v20210629</version>
-    </dependency>
-</dependencies>
